b7d5e59a4cea0434c7a1540403ee39f3b8221107
Affruddhin-sap <affruddhinsap@gmail.com>
2026-09-03 15:30:28 +0530

    Create CI-CD-setup.sh

---
+++ CI-CD-setup.sh
@@ -0,0 +1,8 @@
+first create a instance as jenkins-server to connect jenkins as CI/CD pipeline to automatic the Build and test 
+connect to server :
+                    install java 
+                    install the jenkins from the jenkins.io -> download -> redhate
+                    start the jenkins in command line 
+                    enable the jenkins
+                    copy the public key and paste in chrome with :8080 port number
+second create one job in jenkins to 
